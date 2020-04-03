b29cb98851cfdd9f48394e95506f3bbc769ddcf5
恪晨 <bo.wang1016@outlook.com>
2020-04-03 18:08:41 +0800

    :package:delete hard-source-webpack-plugin

--- src/public/article/AddArticle.js
+++ src/public/article/AddArticle.js
@@ -6,14 +6,45 @@
  * @github https://github.com/BoWang816
  */
 import React, { Component } from 'react';
+import { Modal, Button } from "antd";
 
 export default class ArticleList extends Component {
+	state = { visible: false };
+	showModal = () => {
+		this.setState({
+			visible: true,
+		});
+	};
+
+	handleOk = e => {
+		this.setState({
+			visible: false,
+		});
+	};
+
+	handleCancel = e => {
+		this.setState({
+			visible: false,
+		});
+	};
 
 	render() {
 		return (
 			<div>
-				我是新增文章
+				<Button
+					type="primary"
+					onClick={this.showModal}>
+					Open
+					Modal
+				</Button>
+				<Modal
+					title="Basic Modal"
+					visible={this.state.visible}
+					onOk={this.handleOk}
+					onCancel={this.handleCancel}>
+					<Button onClick={this.open}>再次</Button>
+				</Modal>
 			</div>
 		);
-	};
+	}
 }
