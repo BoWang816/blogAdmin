a72e8a83ea4281bf956c62528f74ce0ccdd51e14
恪晨 <bo.wang1016@outlook.com>
2020-06-12 15:35:14 +0800

    :package: delete npmrc-taobao

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -10,6 +10,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const WebpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+
 const smp = new SpeedMeasurePlugin();
 
 const commonConfig = require('./webpack-common.config');
@@ -38,7 +39,7 @@ const devConfig = {
 	devServer: {
 		contentBase: path.resolve(__dirname, 'admin'),
 		port: serverConfig.serverPort, // 默认8080
-		quiet: false, // 启用 quiet 后，除了初始启动信息之外的任何内容都不会被打印到控制台，这也意味着来自 webpack 的错误或警告在控制台不可见
+		quiet: true, // 启用 quiet 后，除了初始启动信息之外的任何内容都不会被打印到控制台，这也意味着来自 webpack 的错误或警告在控制台不可见
 		inline: true, // 内联模式开启，false为iframe模式
 		overlay: true, // 当存在编译错误或警告时，在浏览器中显示全屏覆盖。默认情况下禁用。
 		clientLogLevel: 'info', // 日志等级，默认info，可能的值有 none, error, warning 或者 info（默认值），当使用内联模式(inline mode)时，在开发工具(DevTools)的控制台(console)将显示消息
